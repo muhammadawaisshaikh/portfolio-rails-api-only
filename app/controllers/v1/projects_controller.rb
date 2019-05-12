7feb185d2d0535abce2ec7611412f12caab54bfd
Muhammad Awais <ashaikh@creativechaos.co>
2019-05-12 20:51:12 +0500

    projects CRUD done

---
+++ app/controllers/v1/projects_controller.rb
@@ -0,0 +1,43 @@
+class V1::ProjectsController < ApplicationController
+  def index
+    projects = Project.all
+    render json: { projects: projects }, status: :ok
+  end
+
+  def create
+    project = Project.new(project_params)
+
+    if project.save
+      render json:project, status: :created
+    else
+      render json: {errors: project.errors}, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    project = Project.find(params[:id])
+    render json: project, status: :ok
+  end  
+
+  def update
+    project = Project.find(params[:id])
+    if project.update(project_params)
+      render json: project, status: :ok
+    else
+      render json: { errors: project.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    project = Project.find(params[:id])
+    project.destroy
+    head 204
+  end
+
+  private
+  
+  def project_params
+    params.require(:project).permit(:imgUrl, :name, :associationWith, :role)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
     resources :skills
     resources :abouts
     resources :experiences
+    resources :projects
   end
 end
