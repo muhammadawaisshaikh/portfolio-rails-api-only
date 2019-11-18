1d98e15ac8acf849bf1c2c3f86b131a997f15b44
Muhammad Awais <awaisshaikh94@gmail.com>
2019-11-18 18:52:16 +0500

    certifications controller CRUD methods init

--- app/controllers/v1/certifications_controller.rb
+++ app/controllers/v1/certifications_controller.rb
@@ -1,2 +1,42 @@
 class V1::CertificationsController < ApplicationController
+    def index
+        certifications = Certification.all
+        render json: { certifications: certifications }, status: :ok
+    end
+
+    def create
+        certification = Certification.new(certification_params)
+
+    if certification.save
+        render json:certification, status: :created
+    else
+        render json: {errors: certification.errors}, status: :unprocessable_entity
+    end
+    end
+
+    def show
+        certification = Certification.find(params[:id])
+        render json: certification, status: :ok
+    end  
+
+    def update
+        certification = Certification.find(params[:id])
+    if certification.update(certification_params)
+        render json: certification, status: :ok
+    else
+        render json: { errors: certification.errors }, status: :unprocessable_entity
+    end
+    end
+
+    def destroy
+        certification = Certification.find(params[:id])
+        certification.destroy
+        head 204
+    end
+
+    private
+    
+    def certification_params
+        params.require(:certification).permit(:imgUrl)
+    end
 end
