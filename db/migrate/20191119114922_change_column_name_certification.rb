da5ed06014af152d97a2976c514777fa953caac5
Muhammad Awais <awaisshaikh94@gmail.com>
2019-11-19 16:51:16 +0500

    certifications change column Name type becuase its reserved in rails

---
+++ db/migrate/20191119114922_change_column_name_certification.rb
@@ -0,0 +1,5 @@
+class ChangeColumnNameCertification < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :certifications, :type, :kind
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_18_134738) do
+ActiveRecord::Schema.define(version: 2019_11_19_114922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 2019_11_18_134738) do
     t.string "organization"
     t.string "organizationImage"
     t.string "date"
-    t.string "type"
+    t.string "kind"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
